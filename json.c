4ea82a54156695b081c9329db962c761dad6e892
Hailey <hailey@not-an-email-address.fake>
2024-10-15 10:55:52 -0500

    sepprate files now

--- json.c
+++ json.c
@@ -16,46 +16,6 @@
 
 // ========= JSON STUFF
 
-typedef struct JOBJ JOBJ, *JOBJPTR;
-typedef struct JARR JARR, *JARRPTR;
-
-/// @brief parses a json object from the given <string> returns NULL on error
-JOBJPTR jobj_from(const char *str);
-void jobj_free(JOBJPTR t);
-
-typedef enum JOBJ_FIELD_KIND
-{
-    JFK_STR,
-    JFK_NUM,
-    JFK_OBJ,
-    JFK_ARR
-} JOBJ_FIELD_KIND;
-
-typedef struct JOBJ_FIELD_VALUE
-{
-    union
-    {
-        STRING str;
-        double number;
-        JOBJPTR obj;
-        JARRPTR arr;
-    } value;
-
-    JOBJ_FIELD_KIND kind;
-} JOBJ_FIELD_VALUE;
-
-typedef struct JOBJ_FIELD
-{
-    STRING fieldName;
-    JOBJ_FIELD_VALUE value;
-} JOBJ_FIELD, *JOBJ_FIELD_PTR;
-
-typedef struct JARR
-{
-    // list of JOBJ_FIELD_VALUE_PTRs
-    ARRAY_LIST values;
-} JARR, *JARRPTR;
-
 typedef struct JOBJ
 {
     // list of JOBJ_FIELD_PTR
@@ -750,9 +710,9 @@ int main()
 
     JOBJPTR s = jobj_from(json);
 
-    // STRING objStr = {0};
-    // jobj_tostring(s, &objStr);
-    // printf("%s\n", objStr.buf);
+    STRING objStr = {0};
+    jobj_tostring(s, &objStr);
+    printf("%s\n", objStr.buf);
 
     if (s == NULL)
     {
--- json.h
+++ json.h
@@ -1,4 +1,45 @@
 #ifndef JSON_H
 #define JSON_H
 
+#include "string.h"
+
+typedef struct JOBJ JOBJ, *JOBJPTR;
+typedef struct JARR JARR, *JARRPTR;
+
+/// @brief parses a json object from the given <string> returns NULL on error
+JOBJPTR jobj_from(const char *str);
+void jobj_free(JOBJPTR t);
+
+typedef enum JOBJ_FIELD_KIND
+{
+    JFK_STR,
+    JFK_NUM,
+    JFK_OBJ,
+    JFK_ARR
+} JOBJ_FIELD_KIND;
+
+typedef struct JOBJ_FIELD_VALUE
+{
+    union
+    {
+        STRING str;
+        double number;
+        JOBJPTR obj;
+        JARRPTR arr;
+    } value;
+
+    JOBJ_FIELD_KIND kind;
+} JOBJ_FIELD_VALUE;
+
+typedef struct JOBJ_FIELD
+{
+    STRING fieldName;
+    JOBJ_FIELD_VALUE value;
+} JOBJ_FIELD, *JOBJ_FIELD_PTR;
+
+typedef struct JARR
+{
+    // list of JOBJ_FIELD_VALUE_PTRs
+    ARRAY_LIST values;
+} JARR, *JARRPTR;
 #endif // JSON_H
